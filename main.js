1eed3772f3d3a9b6f5db84f1215c0ee82c6ada58
JULIABORTOLUCCIDOSREIS <126165271+JULIABORTOLUCCIDOSREIS@users.noreply.github.com>
2023-10-05 09:06:09 -0300

    em processo de criar sons em botôes

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector() {
+    #som_tecla_pom
+}
+document.querySelector('#som_tecla_pom')
+document.querySelector('som_tecla_pom').play();
+onclick="tocasomPom()"
+
+function tocasomPom () {
+    document.querySelector ('#som_tecla_pom').play ()
+}
